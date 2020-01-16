c47ab86d34a7f54d244d8ab669f4502ad8b8fe4e
Michael Villiger <akirasoft@gmail.com>
2020-01-16 14:10:12 -0600

    changes to gitlab k8s script

--- scripts/gitlab-k8s.sh
+++ scripts/gitlab-k8s.sh
@@ -7,13 +7,9 @@ kubectl apply -f ../manifests/gitlab-service-account.yaml
 #kubectl get secret $(kubectl get secrets|grep default|awk '{print $1}') -o jsonpath="{['data']['ca\.crt']}" | base64 --decode
 kubectl get secret -o jsonpath="{.items[?(@.type==\"kubernetes.io/service-account-token\")].data['ca\.crt']}" | base64 --decode > cert.pem
 K8NAME=$(kubectl config current-context)
+K8SAPI=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')
+K8STOKEN=$(kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep gitlab-admin | awk '{print $1}')|grep "token:"|awk '{print $2}')
 
-
-K8STOKEN=$(kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep eks-admin | awk '{print $1}')|grep "token:"|awk '{print $2}')
-#echo $K8STOKEN
-
-K8SAPI=$(cat aws.kubeconfig|grep "server:"|awk '{print $2}')
-echo $K8SAPI
 curl --header "Private-Token: ${GL_API_TOKEN}" https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/clusters/user \
 -H "Accept: application/json" \
 -H "Content-Type:application/json" \
