57c9c77678cddda5df687154c9f03ff08e3dafe9
Michael Villiger <akirasoft@gmail.com>
2020-01-16 15:24:36 -0600

    changes to gitlab script

--- scripts/gitlab-k8s.sh
+++ scripts/gitlab-k8s.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # creates account for gitlab
 export CI_PROJECT_ID=$1
 export GL_API_TOKEN=$2
@@ -9,8 +10,9 @@ kubectl get secret -o jsonpath="{.items[?(@.type==\"kubernetes.io/service-accoun
 K8NAME=$(kubectl config current-context)
 K8SAPI=$(kubectl config view --minify -o jsonpath='{.clusters[0].cluster.server}')
 K8STOKEN=$(kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep gitlab-admin | awk '{print $1}')|grep "token:"|awk '{print $2}')
+CACERT=$(cat cert.pem | sed ':a;N;$!ba;s/\n/\\r\\n/g')
 
 curl --header "Private-Token: ${GL_API_TOKEN}" https://gitlab.com/api/v4/projects/${CI_PROJECT_ID}/clusters/user \
 -H "Accept: application/json" \
 -H "Content-Type:application/json" \
--X POST --data '{"name":"${K8NAME}", "platform_kubernetes_attributes":{"api_url":"${K8SAPI}","token":"${K8STOKEN}","namespace":"","ca_cert":"$(sed :a;N;$!ba;s/\n/\\r\\n/g cert.pem)"}}'
+-X POST --data '{"name":"'${K8NAME}'", "platform_kubernetes_attributes":{"api_url":"'${K8SAPI}'","token":"'${K8STOKEN}'","namespace":"","ca_cert":"'${CACERT}'"}}'
